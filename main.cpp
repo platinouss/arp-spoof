b932721392e7126e5fe46b8a1936c50aee1a0796
p1atin0uss <70827921+platinouss@users.noreply.github.com>
2021-08-06 03:24:54 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ struct EthArpPacket final {
 void usage() {
     printf("syntax: arp-spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip 2>...]\n");
     printf("sample: arp-spoof wlan0 192.168.10.2 192.168.10.1 192.168.10.1 192.168.10.2\n");
-}    struct libnet_ethernet_hdr* Ether;
+}
 
 int main(int argc, char* argv[]) {
 
